81176b4a92fe0ac56f6e4e93aa2a611fe46a85b8
FelipeK57 <kevinboor5725@gmail.com>
2025-07-14 07:37:04 -0500

    Fix CORS origin configuration to correctly use environment variable

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware to allow CORS
 app.use(
   cors({
-    origin: [`${process.env.ORIGIN_ALLOWED}", "http://localhost:5173`],
+    origin: [process.env.ORIGIN_ALLOWED as string, "http://localhost:5173"],
     credentials: true,
   })
 );
