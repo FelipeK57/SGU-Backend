0f74c6d90a7bd6e844729566af164e1fdcf069f9
FelipeK57 <kevinboor5725@gmail.com>
2025-08-20 10:53:55 -0500

    Update CORS configuration to allow requests from the production URL

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware to allow CORS
 app.use(
   cors({
-    origin: [process.env.ORIGIN_ALLOWED as string, "http://localhost:5173"],
+    origin: [process.env.ORIGIN_ALLOWED as string, "https://sgi-semcon.vercel.app/", "http://localhost:5173"],
     credentials: true,
   })
 );
