8c8cb1ad1250885fa14e0eb1f8f4a01e47218449
FelipeK57 <kevinboor5725@gmail.com>
2025-08-20 11:07:33 -0500

    Refactor CORS middleware to use a callback for origin validation

--- src/index.ts
+++ src/index.ts
@@ -23,12 +23,28 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware to allow CORS
+const allowedOrigins = [
+  process.env.ORIGIN_ALLOWED,   
+  "https://sgi-semcon.vercel.app",
+  "http://localhost:5173",
+];
+
 app.use(
   cors({
-    origin: [process.env.ORIGIN_ALLOWED as string, "https://sgi-semcon.vercel.app", "http://localhost:5173"],
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+app.options("*", cors()); // permitir preflight
 
 app.use(express.json());
 
