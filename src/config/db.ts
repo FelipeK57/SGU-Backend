99f7d6442a7bd1b943b4543aabd32d83653c3e88
FelipeK57 <kevinboor5725@gmail.com>
2025-08-20 09:51:40 -0500

    Refactor database connection to use DATABASE_URL for improved configuration

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,9 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME as string,
-  process.env.DB_USER as string,
-  process.env.DB_PASS as string,
+  process.env.DATABASE_URL as string,
   {
     host: process.env.DB_HOST,
     dialect: "postgres",
