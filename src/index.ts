659fae657dd1bbbd61288bfe33be1b19cfd700da
FelipeK57 <kevinfelipebolanosorozco@gmail.com>
2025-10-03 08:58:52 -0500

    Add user retrieval endpoints and update CORS middleware configuration

--- src/index.ts
+++ src/index.ts
@@ -23,12 +23,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware to allow CORS
-app.use(
-  cors({
-    origin: ["http://localhost:5174", "http://localhost:5173", "http://127.0.0.1:5173"],
-    credentials: true,
-  })
-);
+app.use(cors({}));
 
 app.use(express.json());
 
